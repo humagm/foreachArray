05e9b104edd99c02c48723282b9a7d10fdeb2030
Milan Humagain <10011027@BCS.NET.NZ>
2017-09-14 16:11:16 +1200

    Array

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace foreachArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+         var numbers = new List<int>{1,3,5,7,9,11,20};
+        
+         foreach (var x in numbers){
+             if (x % 2 != 0){
+                 Console.WriteLine(x);
+
+             }
+         }
+              
+             
+         
+        }
+    }
+}
